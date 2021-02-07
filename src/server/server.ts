275e69afc06c58fb5fdd47ac592bbdb6cc733605
Will Seese <willxj36@gmail.com>
2021-02-07 16:24:15 -0700

    added cors support primarily so the mobile app can pull data

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,9 +4,11 @@ import * as path from 'path';
 import * as passport from 'passport';
 import './middleware/localstrategy';
 import './middleware/bearerstrategy';
+import * as cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(passport.initialize());
