83be3c2979e0dc8a0ba4df4283ffefbd56fe4e3c
Will Seese <willxj36@gmail.com>
2021-02-09 14:17:16 -0700

    fixed a typo that crept in

--- src/server/db/queries/blogs.ts
+++ src/server/db/queries/blogs.ts
@@ -5,7 +5,7 @@ const all = async () => await Query('SELECT b.id, b.title, b.content, b.authorid
 const one = async (id: number) => await Query('SELECT b.title, b.content, b.authorid, a.name as author, t.name as tag, b._created FROM blogs b JOIN authors a ON a.id = b.authorid JOIN blogtags bt ON bt.blogid = b.id JOIN tags t ON t.id = bt.tagid WHERE b.id = ?', [id]);
 
 const put = async (title: string, content: string, tags: string, id: number) => {
-    await Query('UPDATE blogs SET title = ?, content = ? WHERE id = ?', [title, content, id]);
+    await Query('UPDATE blogs SET title = ?, content = ? WHERE id = ? AND authorid = ?', [title, content, id]);
     await Query('UPDATE blogtags SET tagid = (SELECT id FROM tags WHERE name LIKE ?) WHERE blogid = ?', [tags, id]);
 }
 
@@ -18,7 +18,7 @@ const post = async (title: string, content: string, authorid: string, tags: stri
 
 const deleter = async (id: number) => {
     Query('DELETE FROM blogtags WHERE blogid = ?', [id]);
-    Query('DELETE FROM blogs WHERE id = ?', [id]);
+    Query('DELETE FROM blogs WHERE id = ? AND authorid = ?', [id]);
 }
 
 export default { all, one, put, post, deleter };
--- src/server/routes/api/permissions.ts
+++ src/server/routes/api/permissions.ts
@@ -9,7 +9,7 @@ export const isAuthor: RequestHandler = (req: any, res, next) => {
 };
 
 export const isAdmin: RequestHandler = (req: any, res, next) => {
-    if(!req.user || (req.user.rolem!== 'admin' && req.user.role !== 'webmaster')) {
+    if(!req.user || (req.user.role !== 'admin' && req.user.role !== 'webmaster')) {
         return res.sendStatus(401);
     } else {
         return next();
